ed46c8770e18923532ee6fe9b8c8dc90146b719f
Muzammil Farooq <muzammil.farooq@intagleo.com>
2021-05-26 14:59:40 +0500

    Sample API generated

---
+++ PredatarAPI/Controllers/Sample.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PredatarAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PredatarAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Sample : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<ResponseModel> Get()
+        {
+            var response = new ResponseModel();
+            response.ResponseMessage = "Welcome to Predatar!";
+            return response;
+        }
+    }
+}
---
+++ PredatarAPI/Models/ResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PredatarAPI.Models
+{
+    public class ResponseModel
+    {
+        public string ResponseMessage { get; set; }
+    }
+}
